6ea04d79c2f431872638972ff5d5b90029a44032
Mohammed Mesbah Uddin <52214989+fexydelaide@users.noreply.github.com>
2020-04-14 17:06:41 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,5 +1,5 @@
 from Inventory import Inventory
-from view import View
+from mainmenu import View
 
 
 class Controller:
@@ -9,20 +9,6 @@ class Controller:
 
     def main(self):
         self.view.main()
-
-    def on_button_click(self, item):
-        myInv = Inventory()
-        if item == 'Add Chicken':
-            myInv.addItem("chicken", 45)
-            myInv.__str__()
-        elif item == 'Add Carrots':
-            myInv.addItem("carrots", 15)
-            myInv.__str__()
-        elif item == 'Add Pasta':
-            myInv.addItem("pasta", 22)
-            myInv.__str__()
-        # elif btn_1 == 'Check chicken quantity':
-        # myInv.currQuantity("chicken")
 
 
 if __name__ == '__main__':
